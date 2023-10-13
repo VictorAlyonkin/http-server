5bc6b48c3a41341a4cc9b4234d36ccd5dc70d717
Victor Alyonkin <viktoalenkin@ya.ru>
2023-10-13 15:18:06 +0300

    add Handlers

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import java.io.BufferedOutputStream;
 import java.util.List;
 
 public class Main {
@@ -12,8 +13,25 @@ public class Main {
                 "/classic.html", "/events.html", "/events.js");
 
         Server server = new Server(port, validPaths, numberThreads);
+        addHandlersToTheServer(server);
         server.start();
     }
+
+    public static void addHandlersToTheServer(Server server) {
+        server.addHandler("GET", "/messages",
+                (Request request, BufferedOutputStream out) -> {
+                    String response = "Hello GET/messages. Good luck";
+                    String successfulAnswer = server.getSuccessfulAnswerWithoutMimeType(response.length());
+                    out.write(successfulAnswer.getBytes());
+                    out.write(response.getBytes());
+                    out.flush();
+                });
+        server.addHandler("POST", "/messages", (request, out) -> {
+            String response = "Hello POS/messages. Good luck";
+            String successfulAnswer = server.getSuccessfulAnswerWithoutMimeType(response.length());
+            out.write(successfulAnswer.getBytes());
+            out.write(response.getBytes());
+            out.flush();
+        });
+    }
 }
-
-
